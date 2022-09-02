b0d5212f843060b4bc9df2f78f08f96e62cc99b9
jrccdevs <112733020+jrccdevs@users.noreply.github.com>
2022-09-02 18:42:36 -0400

    initial commit

---
+++ js/prueba.js
@@ -0,0 +1,14 @@
+alert("hola")
+function initMap(){
+    const ubicacion = new Localizacion(()=>{
+        const options = {
+            center: {
+                lat: ubicacion.latitude,
+                lng: ubicacion.longitude 
+            },
+            zoom: 15
+        }
+        var map = document.getElementById('map');
+        const mapa = new google.maps.Map(map, options);
+    });
+}
